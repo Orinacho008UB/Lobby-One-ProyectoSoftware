2714483f31906642ee808b0a39988d200f4f4461
Orinacho008UB <219369125+Orinacho008UB@users.noreply.github.com>
2026-05-22 21:37:42 -0400

    Creacion de Spring MVC y Angular
    Si se ven que son demasiadas carpetas y otras cosas son del propio framework que los crea no hay que borrarlos

---
+++ LobbyOne-Backend/src/main/java/com/LobbyOne_Backend/LobbyOneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.LobbyOne_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LobbyOneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LobbyOneBackendApplication.class, args);
+	}
+
+}
---
+++ LobbyOne-Backend/src/test/java/com/LobbyOne_Backend/LobbyOneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LobbyOne_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LobbyOneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
